11262b8d674e3ee3712395b14ece55021a37f0f3
Doğukan Ürker <dogukanurker@icloud.com>
2026-01-18 22:57:30 +0300

    4 space tab

--- frontend/form/vite.config.ts
+++ frontend/form/vite.config.ts
@@ -2,13 +2,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		sourcemap: true,
+	},
 });
