d0d9a9a7e303c2c5ef552b202e6709604a2a2a8f
Fr0zennx <burakkaya4750@gmail.com>
2026-04-06 19:48:45 +0300

    feat: update biome configuration and improve file structure for consistency

--- frontend/gdg-website/src/App.tsx
+++ frontend/gdg-website/src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 const HomePage = lazy(() => import("./pages/Home/HomePage"));
